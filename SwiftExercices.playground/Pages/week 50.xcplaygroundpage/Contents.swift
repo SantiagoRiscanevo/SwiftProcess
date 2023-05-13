8d8a84dcf442e85721b33fdb8c6a464b3d425811
Santiago Tovar <santiagoriscanevo@icloud.com>
2023-05-13 13:40:35 -0500

    Updating folder

---
+++ SwiftExercices.playground/Pages/week 50.xcplaygroundpage/Contents.swift
@@ -0,0 +1,27 @@
+import Foundation
+
+/*
+ * Crea un programa que calcule quien gana más partidas al piedra,
+ * papel, tijera, lagarto, spock.
+ * - El resultado puede ser: "Player 1", "Player 2", "Tie" (empate)
+ * - La función recibe un listado que contiene pares, representando cada jugada.
+ * - El par puede contener combinaciones de "🗿" (piedra), "📄" (papel),
+ *   "✂️" (tijera), "🦎" (lagarto) o "🖖" (spock).
+ * - Ejemplo. Entrada: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Resultado: "Player 2".
+ * - Debes buscar información sobre cómo se juega con estas 5 posibilidades.
+ */
+
+/*
+Piedra > Lagarto, Tijera
+Papel > Piedra, Spock
+Tijera > papel, lagarto
+Lagarto > Spock, papepl
+Spock > Piedra, tijera
+ */
+
+func game(){
+    
+}
+
+game()
+
